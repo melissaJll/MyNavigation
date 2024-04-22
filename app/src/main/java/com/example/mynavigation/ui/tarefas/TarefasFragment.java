6ed1daa657734b8fbbaff4f3e690a266281943e5
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-04-21 22:28:15 -0300

    novos items ao menu

---
+++ app/src/main/java/com/example/mynavigation/ui/tarefas/TarefasFragment.java
@@ -0,0 +1,21 @@
+package com.example.mynavigation.ui.tarefas;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mynavigation.R;
+
+public class TarefasFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tarefas, container, false);
+    }
+}
