7c98597343d3b187d41d7352b62bbbdc6811ff15
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-04-24 19:54:18 -0300

    Update NotificationsViewModel.java

--- app/src/main/java/com/example/mynavigation/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/example/mynavigation/ui/notifications/NotificationsViewModel.java
@@ -10,7 +10,7 @@ public class NotificationsViewModel extends ViewModel {
 
     public NotificationsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Notificações");
+        mText.setValue("Notificações do App");
     }
 
     public LiveData<String> getText() {
